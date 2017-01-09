cadf39f117d8378686d297a075aceadbf747c05e
Olgierd Nowakowski <olgierd@novakovsky.eu>
2017-01-09 11:17:14 +0100

    working on loading sectors

--- boot.asm
+++ boot.asm
@@ -1,19 +1,30 @@
 bits 16
-
-fast_a20:
-  mov al, 0x92
-  test al, 0x02
-
-
 [ORG 0x7c00]
 
-  jmp fast_a20
-
-  jmp main
 
 main:
+  xor ax,ax
 
-  jmp main
+  mov si, welcometext
+  call printmsg
+
+
+
+
+printmsg:
+  lodsb
+  or al,al
+  jz done
+  mov ah,0x0E
+  int 0x10
+  jmp printmsg
+
+done:
+  ret
+
+
+welcometext db "Welcome to neutron bootloader!",13,10,0
+
 
   times 510-($-$$) db 0
   db 0x55
